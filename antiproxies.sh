c6df03ed1c737821f849943c6c70631869df97c9
Kobunix <kobunix@gmail.com>
2022-11-26 22:51:16 +0100

    Create antiproxies.sh

---
+++ antiproxies.sh
@@ -0,0 +1,143 @@
+#!/bin/sh
+
+
+# THIS SCRIPT IS FREE FOR EVERYONE
+# HOWEVER YOU NEED TO RESPECT THE AUTHOR OF THIS SCRIPT
+# KEEP CREDITS AND MENTION THE USE OF THE SCRIPT
+#
+# Created by Kobunix <help@rootwork.eu>
+#
+
+# ================================================================
+WHITE='\033[1;37m'
+RED='\033[0;31m'
+NC='\033[0m'
+Black='\033[0;30m'
+Green='\033[0;32m'
+Yellow='\033[0;33m'
+Blue='\033[0;34m'
+Purple='\033[0;35m'
+Cyan='\033[0;36m'
+White='\033[0;37m'
+
+
+BASE_APPS='curl ipset iptables iptables-persistent'
+# ================================================================
+
+clear
+
+Error () {
+	
+	# echo "${RED}[Error] ${WHITE}Please choose a valid method : $0 <tor/hostings>${NC}"
+	# echo "${Green}#                                                                           #"
+	echo "${Green}#                            [Available Options]                            #"
+	# echo "${Green}#                                                                           #"
+	echo "${Green}============================================================================="
+	echo "${Green}#                                                                           #${NC}"
+	echo "${Green}#${WHITE}    --install         Install the needed packages\t\t\t    ${Green}#"
+	echo "${Green}#${WHITE}    --uninstall       Uninstall GSAP\t\t\t\t\t    ${Green}#"
+	echo "${Green}#${WHITE}    --updatetor       Update tor Exit nodes\t\t\t            ${Green}#"
+	echo "${Green}#                                                                           #"
+	echo "${Green}#${WHITE}    tor4              Apply tor network ban\t\t\t            ${Green}#"
+	echo "${Green}#${WHITE}    tor6              Apply tor network ban\t\t\t            ${Green}#"
+	echo "${Green}#${WHITE}    hostings          Apply hostings ban\t\t\t            ${Green}#"
+	echo "${Green}#                                                                           #"
+	echo "${Green}=============================================================================${NC}"
+}
+
+UninstallGSAP () {
+
+	echo "${RED}[Uninstall] ${WHITE}Uninstalling ipset rules. ${NC}"
+	ipset destroy tornetv4 > /dev/null 2>&1
+	ipset destroy tornetv6 > /dev/null 2>&1
+
+}
+
+TOR_Rules () {
+	iptables -A INPUT -m set --match-set tornet src -j DROP
+}
+
+TOR_Update_IPV4 () {
+	curl 'https://lists.fissionrelays.net/tor/exits-ipv4.txt' | xargs -n 1 ipset -A tornetv4 > /dev/null 2>&1
+	curl 'https://lists.fissionrelays.net/tor/relays-ipv4.txt' | xargs -n 1 ipset -A tornetv4 > /dev/null 2>&1
+}
+
+TOR_Update_IPV6 () {
+	curl 'https://lists.fissionrelays.net/tor/exits-ipv6.txt' | xargs -n 1 ipset -A tornetv6 > /dev/null 2>&1
+	curl 'https://lists.fissionrelays.net/tor/relays-ipv6.txt' | xargs -n 1 ipset -A tornetv6 > /dev/null 2>&1
+}
+
+Install_Prereqs () {
+	echo "${RED}[Install] ${WHITE}Checking for system updates ${NC}"
+	apt-get update > /dev/null 2>&1
+	echo "${RED}[Install] ${WHITE}Applying system upgrade (if available)${NC}"
+	apt-get upgrade -y > /dev/null 2>&1
+	echo "${RED}[Install] ${WHITE}Installing required APPS : '${BASE_APPS}' ${NC}"
+	apt-get install -y ${BASE_APPS} > /dev/null 2>&1
+	
+	echo "${RED}[Install] ${WHITE}Creating ipset entry : tornetv4 ${NC}"
+	ipset create tornetv4 iphash
+	echo "${RED}[Install] ${WHITE}Creating ipset entry : tornetv6 ${NC}"
+	ipset create tornetv6 iphash
+}
+
+Header () {
+	echo "${Green}=============================================================================${NC}"
+	echo "${Green}#${RED}      ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄               ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄      ${Green}#"
+	echo "${Green}#${RED}     ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌             ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌     ${Green}#"
+	echo "${Green}#${RED}     ▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀▀▀              ▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌     ${Green}#"
+	echo "${Green}#${RED}     ▐░▌          ▐░▌                       ▐░▌       ▐░▌▐░▌       ▐░▌     ${Green}#"
+	echo "${Green}#${RED}     ▐░▌ ▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄█░▌     ${Green}#"
+	echo "${Green}#${RED}     ▐░▌▐░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌     ${Green}#"
+	echo "${Green}#${RED}     ▐░▌ ▀▀▀▀▀▀█░▌ ▀▀▀▀▀▀▀▀▀█░▌ ▀▀▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀▀▀      ${Green}#"
+	echo "${Green}#${RED}     ▐░▌       ▐░▌          ▐░▌             ▐░▌       ▐░▌▐░▌               ${Green}#"
+	echo "${Green}#${RED}     ▐░█▄▄▄▄▄▄▄█░▌ ▄▄▄▄▄▄▄▄▄█░▌             ▐░▌       ▐░▌▐░▌               ${Green}#"
+	echo "${Green}#${RED}     ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌             ▐░▌       ▐░▌▐░▌               ${Green}#"
+	echo "${Green}#${RED}      ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀               ▀         ▀  ▀                ${Green}#"
+	echo "${Green}=============================================================================${NC}"
+	echo "${Green}#${White}                        Game Service - Anti Proxies                        ${Green}#"
+	echo "${Green}#${White}                               Version 1.0.0                               ${Green}#"
+	echo "${Green}#${White}                      By Kobunix <kobunix@rootwork.eu>                     ${Green}#"
+	echo "${Green}=============================================================================${NC}"
+}
+
+
+
+if [ -z "$1" ]; then
+	Header
+	Error
+else
+	echo " "
+	
+	if [ "$1" = "--updatetor" ]; then
+		echo "${RED}[tor4] ${WHITE}Updating list ${NC}"
+		TOR_Update_IPV4 > /dev/null 2>&1
+		TOR_Update_IPV6 > /dev/null 2>&1
+	
+	elif [ "$1" = "--install" ]; then
+		Install_Prereqs
+
+	elif [ "$1" = "--uninstall" ]; then
+		UninstallGSAP
+
+	else
+		if [ "$1" = "tor4" ]; then
+			echo "${RED}[tor4] ${WHITE}Updating list ${NC}"
+			TOR_Update_IPV4 > /dev/null 2>&1
+			sleep 3
+			echo "${RED}[tor4] ${WHITE}Applying rules ${NC}"
+			iptables -A INPUT -m set --match-set tornetv4 src -j DROP
+
+	    elif [ "$1" = "tor6" ]; then
+
+	    	echo "filled tor"
+
+	    elif [ "$1" = "hostings" ]; then
+			echo "filled hostings"
+		
+		else
+			Error
+		fi
+	fi
+	echo " "
+fi
